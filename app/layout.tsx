87b17650b43547b5c0df1538e34b31217ceb7914
Raju <rajankhanalxz@gmail.com>
2026-07-08 18:46:30 +0545

    feat: clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,15 @@
 import type { Metadata } from "next"
 import { Space_Grotesk, JetBrains_Mono } from "next/font/google"
 import "./globals.css"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
+import {
+    ClerkProvider,
+    Show,
+    SignInButton,
+    SignUpButton,
+    UserButton,
+} from "@clerk/nextjs"
 
 const spaceGrotesk = Space_Grotesk({
     variable: "--font-space-grotesk",
@@ -21,11 +27,10 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html
-            lang="en"
-            className={cn("h-full", "antialiased", spaceGrotesk.variable, "font-mono", jetbrainsMono.variable)}
-        >
-            <body className="min-h-full flex flex-col">{children}</body>
+        <html lang="en">
+            <body className={`${spaceGrotesk.variable} antialiased`}>
+                <ClerkProvider>{children}</ClerkProvider>
+            </body>
         </html>
     )
 }
