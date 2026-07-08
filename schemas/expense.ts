ba5d29e2a408bcdb9cfad2dfd755a619c5a4fd28
Raju <rajankhanalxz@gmail.com>
2026-07-08 18:47:31 +0545

    feat: add expense validation using zod

---
+++ schemas/expense.ts
@@ -0,0 +1,26 @@
+import * as z from "zod"
+
+export const expenseSchema = z.object({
+    title: z
+        .string()
+        .min(1, "Title is required")
+        .max(100, "Title must be under 100 characters"),
+
+    amount: z
+        .number()
+        .positive("Amount must be greater than 0")
+        .multipleOf(0.01, "Amount can have at most 2 decimal places"),
+
+    description: z
+        .string()
+        .min(1, "Description is required")
+        .max(100, "Description must be under 100 characters"),
+
+    expense_date: z.date(),
+    transaction_type: z.enum(["CASH", "ONLINE"]),
+    userId: z.string().check(z.uuid("Invalid user ID")),
+    categoryId: z.string().check(z.uuid("Invalid category ID")),
+})
+
+// Infer the TypeScript type from the schema
+export type ExpenseFormData = z.infer<typeof expenseSchema>
