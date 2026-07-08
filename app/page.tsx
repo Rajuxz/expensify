0dfc3efd58a368bdedbeb7516f2206fa9bff612e
Raju <rajankhanalxz@gmail.com>
2026-07-08 18:46:42 +0545

    feat: Navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react"
+import Navbar from "@/contents/Navbar"
 
 const Home = () => {
-    return <div>page</div>
+    return (
+        <>
+            <Navbar />
+        </>
+    )
 }
 
 export default Home
