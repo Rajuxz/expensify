e179572765df11131a2f4b4a09b960c1b9ad9b06
Raju <rajankhanalxz@gmail.com>
2026-07-09 23:16:31 +0545

    feat (ui): Layout for dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,8 @@
+import { DashboardShell } from "@/features/dashboard/components/dashboard-shell"
+export default function DashboardLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return <DashboardShell>{children}</DashboardShell>
+}
