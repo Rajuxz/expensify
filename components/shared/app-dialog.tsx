25438b9130b4d6d495f59fd3110ba813b2cb6b41
Raju <rajankhanalxz@gmail.com>
2026-07-10 22:06:36 +0545

    feat (ui): Dialog wrapper

---
+++ components/shared/app-dialog.tsx
@@ -0,0 +1,43 @@
+import React from "react"
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+type AppDialogProps = {
+    trigger: React.ReactElement
+    title?: string
+    description?: string
+    children: React.ReactNode
+}
+
+const AppDialog = ({
+    trigger,
+    title,
+    description,
+    children,
+}: AppDialogProps) => {
+    return (
+        <Dialog>
+            <DialogTrigger render={trigger} />
+            <DialogContent>
+                {(title || description) && (
+                    <DialogHeader>
+                        {title && <DialogTitle>{title}</DialogTitle>}
+                        {description && (
+                            <DialogDescription>{description}</DialogDescription>
+                        )}
+                    </DialogHeader>
+                )}
+                {children}
+            </DialogContent>
+        </Dialog>
+    )
+}
+
+export default AppDialog
