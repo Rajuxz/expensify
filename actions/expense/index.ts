bf28b2247c5541c65871402d42a74b5303e7a9bb
Rajuxz <rajankhanalxz@gmail.com>
2026-08-03 19:19:50 +0545

    feat(expense): refactor createExpense function to include user creation and error handling

--- actions/expense/index.ts
+++ actions/expense/index.ts
@@ -1,13 +1,48 @@
-"user server"
-import { auth } from "@clerk/nextjs/server"
-export async function createExpense(data: FormData) {
-    const { userId } = await auth()
+"use server"
+import { Prisma } from "@/lib/generated/prisma/client"
+import { prisma } from "@/lib/prisma"
+import { ExpenseFormData } from "@/schemas/expense"
+import { auth, currentUser } from "@clerk/nextjs/server"
 
-    if (!userId) {
-        throw new Error("Unauthorized.")
+export async function createExpense(input: ExpenseFormData) {
+    const { userId: clerkId } = await auth.protect()
+
+    let user = await prisma.users.findUnique({ where: { clerk_id: clerkId } })
+    if (!user) {
+        const clerkUser = await currentUser()
+        user = await prisma.users.create({
+            data: {
+                clerk_id: clerkId,
+                username:
+                    clerkUser?.username ??
+                    clerkUser?.emailAddresses[0]?.emailAddress ??
+                    clerkId,
+                avatar_url: clerkUser?.imageUrl,
+            },
+        })
+        return { success: false, error: "User not found" }
     }
 
     // adding expenses in the database.
+    try {
+        const expense = await prisma.expenses.create({
+            data: {
+                title: input.title,
+                amount: input.amount,
+                description: input.description,
+                expense_date: input.expense_date ?? new Date(),
+                transaction_type: input.transaction_type,
+                user: { connect: { id: user.id } },
+                category: { connect: { id: input.categoryId } },
+            },
+        })
 
-    
+        return { success: true, data: expense }
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            // e.g. P2003 = foreign key constraint failed
+            return { success: false, error: "Invalid category or user" }
+        }
+        throw error
+    }
 }
