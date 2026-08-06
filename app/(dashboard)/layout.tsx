6526fceb692d4e20843008d76ddda7066a322a41
Rajuxz <rajankhanalxz@gmail.com>
2026-08-06 09:30:22 +0545

    feat(dashboard): fetch and display monthly expense in DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,12 +1,19 @@
+import { getMonthlyExpense } from "@/actions/expense"
 import { DashboardShell } from "@/features/dashboard/components/dashboard-shell"
 import { Toaster } from "sonner"
-export default function DashboardLayout({
+export default async function DashboardLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+    const now = new Date()
+
+    const year = now.getFullYear()
+    const month = now.getMonth()
+
+    const monthlyAmount = await getMonthlyExpense(year, month)
     return (
-        <DashboardShell>
+        <DashboardShell monthlyAmount={monthlyAmount}>
             <Toaster richColors />
             {children}
         </DashboardShell>
