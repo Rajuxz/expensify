12b74c94eed4629c391579773260ad5f2f21f1d0
Rajuxz <rajankhanalxz@gmail.com>
2026-08-08 13:12:48 +0545

    update (dashboard): adjust padding and margin for expenses header

--- app/(dashboard)/dashboard/transactions/page.tsx
+++ app/(dashboard)/dashboard/transactions/page.tsx
@@ -5,8 +5,8 @@ import { getExpenses } from "@/actions/expense"
 const Transaction = async () => {
     const expenses = await getExpenses()
     return (
-        <div className="p-6">
-            <h1 className="mb-6 text-2xl font-bold">Expenses</h1>
+        <div className="p-2">
+            <h1 className="mb-2 text-2xl font-bold">Expenses</h1>
 
             <DataTable columns={columns} data={expenses} />
         </div>
