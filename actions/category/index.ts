1993bcb0f289f5620215bf4a91ff3a37dd783b15
Rajuxz <rajankhanalxz@gmail.com>
2026-08-13 22:03:18 +0545

    feat(dashboard): refactor category fetching and creation logic for improved user context handling

--- actions/category/index.ts
+++ actions/category/index.ts
@@ -1,14 +1,41 @@
 "use server"
+import requireUser from "@/lib/auth/getCurrentUser"
 import { prisma } from "@/lib/prisma"
-import { unstable_cache } from "next/cache"
+import { revalidateTag, unstable_cache } from "next/cache"
 
-export const getCategories = unstable_cache(
-    async () => {
+export const getCachedCategories = unstable_cache(
+    async (userId: string) => {
         return prisma.categories.findMany({
-            select: { id: true, name: true, icon: false, color: false },
+            where: { userId },
+            select: { id: true, name: true, color: false },
             orderBy: { name: "asc" },
         })
     },
     ["categories"],
     { revalidate: 43200, tags: ["categories"] }
 )
+
+export async function getCategories() {
+    const user = await requireUser()
+    return getCachedCategories(user.id)
+}
+
+export async function createCategory(name: string) {
+    const user = await requireUser()
+    if (!name.trim()) {
+        return { success: false, error: "Category name is required" }
+    }
+
+    try {
+        const category = await prisma.categories.create({
+            data: {
+                name: name.trim(),
+                userId: user.id,
+            },
+        })
+        revalidateTag("categories", "max")
+        return { success: true, data: category }
+    } catch (error) {
+        return { success: false, error: "Category already exists." }
+    }
+}
