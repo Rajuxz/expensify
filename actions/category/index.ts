ec58a83cd7e1a158cc10f186b7da5259d477246b
Rajuxz <rajankhanalxz@gmail.com>
2026-08-16 23:51:00 +0545

    feat(categories): implement update and delete functionality with error handling

--- actions/category/index.ts
+++ actions/category/index.ts
@@ -1,5 +1,6 @@
 "use server"
 import requireUser from "@/lib/auth/getCurrentUser"
+import { Prisma } from "@/lib/generated/prisma/client"
 import { prisma } from "@/lib/prisma"
 import { revalidateTag, unstable_cache } from "next/cache"
 
@@ -33,9 +34,80 @@ export async function createCategory(name: string) {
                 userId: user.id,
             },
         })
-        revalidateTag("categories", "max")
+        revalidateTag("categories", { expire: 0 })
         return { success: true, data: category }
     } catch (error) {
         return { success: false, error: "Category already exists." }
+    }
+}
+
+export async function updateCategory(name: string, id: string) {
+    const user = await requireUser()
+    if (!name.trim() || !id) {
+        return { success: false, error: "Something went wrong." }
+    }
+
+    try {
+        const updatedCategory = await prisma.categories.update({
+            where: { userId: user.id, id: id },
+            data: {
+                name: name,
+            },
+        })
+        revalidateTag("categories", { expire: 0 })
+        return { success: true, data: updatedCategory }
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                return { success: false, error: "Category not found." }
+            }
+        }
+        console.error(error)
+        return { success: false, error: "Cannot update category." }
+    }
+}
+
+export async function deleteCategory(id: string) {
+    const user = await requireUser()
+    if (!id) {
+        return { success: false, error: "Something went wrong." }
+    }
+
+    try {
+        const recordCount = await prisma.expenses.count({
+            where: { userId: user.id, categoryId: id, isDeleted: false },
+        })
+
+        if (recordCount > 0) {
+            return {
+                success: false,
+                error: "There are expenses available. Please recategorized first.",
+            }
+        } else {
+            const category = await prisma.categories.findUnique({
+                where: { id },
+            })
+            if (!category || category.userId !== user.id) {
+                return {
+                    success: false,
+                    error: "You cannot delete the category.",
+                }
+            }
+
+            await prisma.categories.delete({ where: { id } })
+            revalidateTag("categories", { expire: 0 })
+
+            return {
+                success: true,
+                error: "Category deleted successfully.",
+            }
+        }
+    } catch (error) {
+        if (error instanceof Prisma.PrismaClientKnownRequestError) {
+            if (error.code === "P2025") {
+                return { success: false, error: "Category Not Found." }
+            }
+            return { success: false, error: "Cannot Delete category." }
+        }
     }
 }
