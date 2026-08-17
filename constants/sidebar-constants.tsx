1e1257a44bca1137ce1a7f3c4304de6ba77a5f83
Rajuxz <rajankhanalxz@gmail.com>
2026-08-18 01:12:09 +0545

    feat(dashboard) : removing budget debt menu is added.

--- constants/sidebar-constants.tsx
+++ constants/sidebar-constants.tsx
@@ -4,12 +4,13 @@ import {
     Wallet,
     PieChart,
     Settings,
+    DollarSignIcon,
 } from "lucide-react"
 
 export const navItems = [
     { label: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
     { label: "Transactions", href: "/dashboard/transactions", icon: Receipt },
-    { label: "Budgets", href: "/dashboard/budgets", icon: Wallet },
+    { label: "Debts", href: "/dashboard/debts", icon: DollarSignIcon },
     { label: "Reports", href: "/dashboard/reports", icon: PieChart },
     { label: "Settings", href: "/dashboard/settings", icon: Settings },
 ]
