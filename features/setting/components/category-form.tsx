a8d724b3d14260fcd0f82523371a1fb499c375c8
Rajuxz <rajankhanalxz@gmail.com>
2026-09-16 20:05:37 +0900

    fix(category-dialog): change button type to prevent form submission on trigger click

--- features/setting/components/category-form.tsx
+++ features/setting/components/category-form.tsx
@@ -8,9 +8,9 @@ import { z } from "zod"
 import { Plus, Pencil } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-
 import AppDialog from "@/components/shared/app-dialog"
 import { toast } from "sonner"
+
 const categorySchema = z.object({
     name: z.string().min(1, "Category name is required"),
 })
@@ -53,7 +53,7 @@ export function CategoryDialog({
             onOpenChange={setOpen}
             trigger={
                 <Button
-                    type="submit"
+                    type="button"
                     variant="ghost"
                     size="icon"
                     className="cursor-pointer text-green-600 hover:text-green-700 transition-all duration-150"
